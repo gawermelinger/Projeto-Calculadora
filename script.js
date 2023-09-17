6c56abd7e71ed5d3df93e5675231a9f68a49e3ce
Gabi <devgabi@DESKTOP-JBTHNIT>
2023-09-17 13:34:06 -0300

    Fisrt commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let flagFinish = 0;
+const operators = ["+", "-", "x", "÷"];
+function checkLastDigit(currentValue, valueList) {
+  currentValue = valueList.includes(currentValue.slice(-1))
+    ? currentValue.slice(0, -1)
+    : currentValue;
+  return currentValue;
+}
+function clearDisplay() {
+  let display = document.querySelector("span");
+  display.innerText = "";
+  flagFinish = 0;
+}
+function checkLength(input, maxLength) {
+  return input.length > maxLength ? input.slice(0, -1) : input;
+}
+
+const keyNumber = document.querySelectorAll(".number");
+keyNumber.forEach((keyNumber) => {
+  keyNumber.addEventListener("click", () => {
+    flagFinish === 1 ? clearDisplay() : false;
+    let display = document.querySelector("span");
+    const currentValue = checkLength(display.innerText, 8);
+    display.innerText = currentValue + keyNumber.value;
+  });
+});
+
+const keyOperator = document.querySelectorAll(".operator");
+keyOperator.forEach((keyOperator) => {
+  keyOperator.addEventListener("click", () => {
+    flagFinish = 0;
+    let display = document.querySelector("span");
+    if (display.innerText === "") {
+      display.innerText = "0" + keyOperator.value;
+    } else {
+      let currentValue = checkLastDigit(display.innerText, operators);
+      currentValue = checkLength(currentValue, 8);
+      display.innerText = currentValue + keyOperator.value;
+    }
+  });
+});
+
+const keyDot = document.getElementById("dot");
+keyDot.addEventListener("click", () => {
+  flagFinish === 1 ? clearDisplay() : false;
+  let display = document.querySelector("span");
+  if (display.innerText === "") {
+    display.innerText = "0" + keyDot.value;
+  } else {
+    let currentValue = checkLastDigit(display.innerText, ".");
+    currentValue = checkLength(currentValue, 8);
+    display.innerText = currentValue + keyDot.value;
+  }
+});
+
+const keyEqual = document.getElementById("equal");
+keyEqual.addEventListener("click", () => {
+  let display = document.querySelector("span");
+  let operation = display.innerText;
+  if (operation === "") {
+    operation = "0";
+  }
+  operation = checkLastDigit(operation, operators);
+  operation = operation.replace("x", "*").replace("÷", "/");
+  result = eval(operation);
+  display.innerText = result.toFixed(8).toString().substring(0, 10);
+  flagFinish = 1;
+});
+
+const btnClear = document.getElementById("btn-clear");
+btnClear.addEventListener("click", () => {
+  clearDisplay();
+});
